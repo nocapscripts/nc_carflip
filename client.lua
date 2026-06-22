89a2fd8a549ff98441e90c54cafc9eabb5b900d3
nocapscripts <pals.sten@gmail.com>
2026-06-23 02:04:21 +0300

    small remake

--- client.lua
+++ client.lua
@@ -1,9 +1,15 @@
-local Core = nil
+local Core = nil -- NOT NEEDED ??
 local framework = "NONE"
 local cfg = lib.require('config')
 local minDuration = cfg.minDuration
 local maxDuration = cfg.maxDuration
-lib.locale()
+lib.locale(cfg.locale) -- LANG
+
+
+function DebugPrint(message)
+
+    print("^2[nc_carflip]^0 " .. message)
+end
 
 CreateThread(function()
     Wait(5)
@@ -14,6 +20,10 @@ CreateThread(function()
     elseif cfg.core == 'es_extended' then
         if GetResourceState(cfg.ESXCoreName) == 'starting' or GetResourceState(cfg.ESXCoreName) == 'started' then
             framework = "ESX"
+        end
+    elseif cfg.core == 'ncfw' then
+        if GetResourceState(cfg.NCFWCoreName) == 'starting' or GetResourceState(cfg.NCFWCoreName) == 'started' then
+            framework = "NCFW"
         end
     end
 
@@ -27,8 +37,7 @@ end)
 CreateThread(function()
     Wait(5)
 
-    if framework == "QB" then
-
+    if framework == "QB" then -- FOR QB FRAMEWORK
 
         local Core = exports[cfg.QBFramework]:GetCoreObject()
 
@@ -36,7 +45,7 @@ CreateThread(function()
         --Fliperiino event
         RegisterNetEvent('nc_carflip:flipcar')
         AddEventHandler('nc_carflip:flipcar', function()
-            print("Fliping vehicle!")
+            DebugPrint("Fliping vehicle!")
             return FlipCarOver()
         end)
 
@@ -112,15 +121,119 @@ CreateThread(function()
             end
         end
 
-    elseif framework == "ESX" then
+    elseif framework == "ESX" then -- FOR ESX FRAMEWORK
 
         local Core = exports[cfg.ESXCoreName]:getSharedObject()
+        local PlayerData, PlayerJob, PlayerGang = {}, {}, {}
+
+        RegisterNetEvent('esx:playerLoaded')
+        AddEventHandler('esx:playerLoaded',function(xPlayer, isNew, skin)
+
+            PlayerData = xPlayer
+            PlayerJob = xPlayer.job
+            PlayerGang = xPlayer.gang
+        end)
 
 
         --Fliperiino event
         RegisterNetEvent('nc_carflip:flipcar')
         AddEventHandler('nc_carflip:flipcar', function()
-            print("Fliping vehicle!")
+            DebugPrint("Fliping vehicle!")
+
+            return FlipCarOver()
+        end)
+
+
+
+
+        -- Fliperiino function can be better
+        function FlipCarOver()
+            local ped = PlayerPedId()
+            local pedcoords = GetEntityCoords(ped)
+
+            local VehicleData = Core.Game.GetClosestVehicle()
+            print(json.encode(VehicleData))
+
+            local finalDur = math.random(minDuration, maxDuration)
+
+            local dist = #(pedcoords - GetEntityCoords(VehicleData))
+            local success = lib.skillCheck({'easy', 'easy', {areaSize = cfg.areaSize, speedMultiplier = cfg.speedMultiplier}, 'hard'}, {'x', 'x', 'x', 'x'})
+
+
+            if dist <= 3  and IsVehicleOnAllWheels(VehicleData) == false then
+                if success then
+
+                    RequestAnimDict('missfinale_c2ig_11')
+                    while not HasAnimDictLoaded("missfinale_c2ig_11") do
+                        Wait(10)
+                    end
+                    lib.progressCircle({
+                        label = locale('notify.fliping'),
+                        duration = finalDur,
+                        position = cfg.position,
+                        useWhileDead = cfg.useWhileDead,
+                        canCancel = cfg.canCancel,
+                        disable = {
+                            car = cfg.disable,
+                        },
+                        anim = {
+                            dict = 'missfinale_c2ig_11',
+                            clip = 'pushcar_offcliff_m'
+                        },
+                    })
+
+                    local carCoords = GetEntityRotation(VehicleData, 2)
+                    SetEntityRotation(VehicleData, carCoords[1], 0, carCoords[3], 2, true)
+                    SetVehicleOnGroundProperly(VehicleData)
+                    lib.notify({
+                        id = '1',
+                        title = locale('notify.title'),
+                        description = locale('notify.desc'),
+                        position = cfg.position,
+                        style = {
+                            backgroundColor = '#243661',
+                            color = '#909296'
+                        },
+                        icon = 'car',
+                        iconColor = '#C53030'
+                    })
+                    ClearPedTasks(ped)
+                end
+            elseif IsVehicleOnAllWheels(VehicleData) ~= false then
+                lib.notify({
+                    id = '1',
+                    title = locale('notify.title'),
+                    description = locale('notify.fail'),
+                    position = cfg.position,
+                    style = {
+                        backgroundColor = '#243661',
+                        color = '#D2D2D2'
+                    },
+                    icon = 'warning',
+                    iconColor = '#C53030'
+                })
+
+            end
+        end
+    elseif framework == "NCFW" then
+
+        local Core = exports[cfg.NCFWCoreName]:getSharedObject()
+        local PlayerData, PlayerJob, PlayerGang = {}, {}, {}
+
+        RegisterNetEvent('nc:playerLoaded')
+        AddEventHandler('nc:playerLoaded',function(xPlayer, isNew, skin)
+
+            PlayerData = xPlayer
+            PlayerJob = xPlayer.job
+            PlayerGang = xPlayer.gang
+        end)
+
+
+        --Fliperiino event
+        RegisterNetEvent('nc_carflip:flipcar')
+        AddEventHandler('nc_carflip:flipcar', function()
+            DebugPrint("Fliping vehicle!")
+
             return FlipCarOver()
         end)
 
--- config.lua
+++ config.lua
@@ -1,17 +1,20 @@
 return {
-    core = 'es_extended', -- choose core between 'qb-core' and 'es_extended'
+    locale = 'en',
+
+    core = 'es_extended', -- choose core between 'qb-core' and 'es_extended' or 'ncfw'
 
     QBFramework = 'qb-core', -- custom resource exports name
-    ESXCoreName = 'ncfw', -- custom resource exports name
+    ESXCoreName = 'es_extended', -- custom resource exports name default: 'es_extended'
+    NCFWCoreName = 'ncfw', -- custom resource exports name default: 'es_extended'
 
     -- OX SETTINGS
 
-    position = 'bottom',
-    minDuration = 5000,
-    maxDuration = 10000,
-    canCancel = false,
-    useWhileDead = false,
-    disable = true,
-    areaSize = 30,
-    speedMultiplier = 1,
+    position = 'bottom', -- ui position
+    minDuration = 5000, -- minimum duration in milliseconds
+    maxDuration = 10000, -- maximum duration in milliseconds
+    canCancel = false, -- whether the notification can be cancelled
+    useWhileDead = false, -- whether the notification can be used while dead
+    disable = true, -- whether the notification is disabled
+    areaSize = 30, -- area size in pixels
+    speedMultiplier = 1, -- speed multiplier for skills
 }
